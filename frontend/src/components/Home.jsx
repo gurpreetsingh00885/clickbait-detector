170b0569c8d4b7b65ef2a1521d7352e233d2aa5f
Gurpreet Singh <gurpreetsingh00885@gmail.com>
2019-03-18 22:16:09 +0530

    Add page styles

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,11 +1,49 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Loader from 'react-loader-spinner';
 // https://api.fakenewsdetector.org/votes_by_content?content=Protests against Donald Trump have occurred in the United States, Europe and elsewhere since his entry into the 2016 presidential campaign
 //https://api.fakenewsdetector.org/votes?url=https://en.wikipedia.org/wiki/Al_Arabiya&title=
+
+const results = props => (<h1> Results </h1>);
+
+
+const classes = {
+  parent: {
+    "display" : "flex",
+    "flex-direction" : "row",
+    "width" : "50%",
+    "border" : "1px dotted black",
+    "border-radius" : "5px",
+    "padding" : "10px",
+    "background-color" : "lightgray"
+  },
+  column1: {
+    "flex" : "1 1 0px",
+    "text-align" : "initial",
+    "padding-left" : "15px"
+  },
+  column2: {
+    "flex" : "1 1 0px",
+    "text-align" : "initial",
+    "border-left" : "1px dotted black",
+    "padding-left" : "15px"
+  },
+  textbox: {
+    "width" : "500px",
+    "height" : "40px",
+    "padding-right": "20px"
+  },
+  submit: {
+    height : "40px"
+  }
+};
+
+
 class Home extends Component {
   state = {
-    data: false,
-    content: ""
+    data: "",
+    content: "",
+    working: false
   }
 
   isValidURL = (str) => {
@@ -19,21 +57,30 @@ class Home extends Component {
   }
 
   process = (e) => {
+    this.setState({data : ""});
     e.preventDefault();
     const data = new FormData(e.target);
     const content = data.get('content');
+    var url = "";
     if(this.isValidURL(content)) {
-      const url = 'https://api.fakenewsdetector.org/votes?url='+content+'&title=';
-      axios.get(url)
-      .then((response) => console.log(response))
-      .catch((err) => console.log(err));
+      url = 'https://api.fakenewsdetector.org/votes?url='+content+'&title=';
     }
     else {
-      const url = 'https://api.fakenewsdetector.org/votes_by_content?content='+content;
-      axios.get(url)
-      .then((response) => console.log(response))
-      .catch((err) => console.log(err));
+      url = 'https://api.fakenewsdetector.org/votes_by_content?content='+content;
     }
+    this.setState({working: true});
+    axios.get(url)
+      .then((response) => { 
+          this.setState({ data:JSON.stringify(response.data, null, 4) })
+          this.setState({working: false});
+        }
+      )
+      .catch((err) => {
+          this.setState({ data:JSON.stringify(err, null, 4) })
+          this.setState({working: false});
+        }
+      );
+
   }
 
   onContentChange = (e) => {
@@ -42,28 +89,62 @@ class Home extends Component {
 
   render() {
     return (
-      <div>
+      <div style={classes.base}>
         <center>
           <h1> Fake News Detector </h1>
           <h3>
             Team Dirty Bits' Deloitte Technoutsav 2.0 project
           </h3>
+
+          This is a demo website for our project. Just enter some text or a link in the text box below and press the button for the magic.
+
+          <br/><br/>
+          <form onSubmit={this.process} action="" method="POST">
+            <input
+              required={true}
+              type="text"
+              onChange={this.onContentChange}
+              name="content"
+              style={classes.textbox}
+            />
+            &nbsp;&nbsp;
+            <input
+              type="submit"
+              disabled={this.state.working}
+              style={classes.submit}
+              value="Analyze"
+            />
+          </form>
+
+          <br/><br/>
+          {
+            this.state.data !== "" && (
+              <div style = {classes.parent}>
+                <div style = {classes.column1}>
+                  <pre>
+                    {this.state.data}
+                  </pre>
+                </div>
+                <div style = {classes.column2}>
+                  <results />
+                </div>
+              </div>
+            )
+          }
+          {
+            this.state.working &&
+            <div>
+              <div style={{"height" : "100px"}} />
+              <Loader 
+                type="Puff"
+                color="#0f0f0f"
+                height="100" 
+                width="100"
+              />
+            </div>
+          }
+          
         </center>
-
-        This is a demo website for our project. Just enter some text or a link in the text box below and press the button for the magic.
-
-        <br/><br/>
-        <form onSubmit={this.process} action="" method="POST">
-          <input
-            required={true}
-            type="text"
-            onChange={this.onContentChange}
-            name="content"
-          />
-          <input type="submit"/>
-        </form>
-
-        
       </div>
     );
   }
